4ddd9a35f49ae8f3b8ab2ace908cee8ab370cf81
eunseo5355 <55755686+eunseo5355@users.noreply.github.com>
2020-06-10 21:19:35 +0900

    2020. 06. 10.

--- 6강 과제.py
+++ 6강 과제.py
@@ -6,49 +6,45 @@ class human:
         self.MP = MP
 
     def meet(self):
-        print("name: %s" % self.name)
-        print("HP: %d" % self.HP)
-        print("MP: %d" % self.MP)
         print("안녕하세요")
 
 
 class player(human):
-    def __init__(self, name, HP, MP, AD):
-        super().__init__(name, HP, MP)
-        self.AD = AD
+    AD = 100
 
-    def att(self):
-        enemy.HP = int(self.AD) - int(enemy.DP)
+    def att(self, enemy):
+        if self.MP >= 50:
+            enemy.HP -= self.AD - enemy.DP
+            self.MP -= 50
+            print("공격성공")
+            print("플레이어의 마나는", self.MP, "입니다.")
+            print("적의 체력은", enemy.HP, "입니다.")
+        else:
+            print("마나가 적습니다.")
 
-
-class npc:
     def meet(self):
         print("반갑습니다")
 
 
+class npc(human):
+    pass
+
+
 class enemy(human):
-    DP = None
-
-    def __init__(self, name, HP, MP, DP):
-        super().__init__(name, HP, MP)
-        self.DP = DP
-
-    def getEnemyDP(self):
-        return self.DP
-
-    def getEnemyHP(self):
-        return self.HP
+    DP = 50
 
 
-p1 = player("eunseo", 100, 100, 50)
-e1 = enemy("aaa", 70, 30, 20)
-hi = npc()
+p1 = player("player", 100, 100)
+e1 = enemy("enemy", 100, 100)
+npc1 = npc("npc", 100, 100)
 
-while int(e1.getEnemyHP()) != 0:
-    num = int(input("1 또는 2 입력:"))
-    if num == 1:
+while True:
+    num = input("npc와 만나려면 1, 공격하려면 2 :")
+    if num == '1':
         p1.meet()
-        hi.meet()
-    elif num == 2:
-        p1.att()
-        print(e1.getEnemyHP())
+        npc1.meet()
+    elif num == '2':
+        p1.att(e1)
+        if e1.HP <= 0:
+            break
+
